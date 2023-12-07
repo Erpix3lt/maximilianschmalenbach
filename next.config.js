739bc897aef7fda0b5e64eaab6a2ba92c8857096
Maximilian Schmalenbach <34858184+Erpix3lt@users.noreply.github.com>
2023-12-07 09:46:44 +0100

    removed console log

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ function removeHttps(url) {
     return url.replace(/^https:\/\//, '');
 }
 
-console.log("NEXT_PUBLIC_SUPABASE_URL", removeHttps(process.env.NEXT_PUBLIC_SUPABASE_URL))
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
